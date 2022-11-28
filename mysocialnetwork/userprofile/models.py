b0e60d2746d00c27878f13516723e7345ef1d1cb
haleck <haleckiydenisirl@gmail.com>
2022-11-28 18:17:32 +0700

    Some changes in models

--- mysocialnetwork/userprofile/models.py
+++ mysocialnetwork/userprofile/models.py
@@ -4,29 +4,31 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=50)
     status = models.TextField(max_length=200)
-    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", null=True)
 
     def __str__(self):
         return self.name + f', user with id: {self.pk}'
 
 
 class Points(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
 
 
 class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField(max_length=800)
     photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
 
 
 class Comments(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
     text = models.TextField(max_length=400)
 
 
 class Photos(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     time_create = models.DateTimeField(auto_now_add=True)
     photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
 
