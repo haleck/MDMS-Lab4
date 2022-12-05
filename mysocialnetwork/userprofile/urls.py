3d1282e8dcd0fb779ea1b7d3a5ebf9d7971ee164
haleck <haleckiydenisirl@gmail.com>
2022-12-05 16:44:23 +0700

    Added CRUD for all models

--- mysocialnetwork/userprofile/urls.py
+++ mysocialnetwork/userprofile/urls.py
@@ -2,7 +2,19 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.UserAPI.as_view(), name='userProfile'),
-    path('<int:pk>/', views.UserAPI.as_view(), name='userProfile'),
+    path('api/users/', views.UserAPICreate.as_view()),
+    path('api/users/<int:pk>/', views.UserAPIUpdate.as_view()),
+
+    path('api/posts/', views.PostAPICreate.as_view()),
+    path('api/posts/<int:pk>/', views.PostAPIUpdate.as_view()),
+
+    path('api/points/', views.PointsAPICreate.as_view()),
+    path('api/points/<int:pk>/', views.PointsAPIUpdate.as_view()),
+
+    path('api/comments/', views.CommentsAPICreate.as_view()),
+    path('api/comments/<int:pk>/', views.CommentsAPIUpdate.as_view()),
+
+    path('api/photos/', views.PhotosAPICreate.as_view()),
+    path('api/photos/<int:pk>/', views.PhotosAPIUpdate.as_view()),
 ]
 
--- mysocialnetwork/userprofile/views.py
+++ mysocialnetwork/userprofile/views.py
@@ -7,14 +7,54 @@ from .models import *
 from .serializers import *
 
 
-class UserAPI(generics.RetrieveUpdateDestroyAPIView):
+class UserAPIUpdate(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
-class PostAPI(generics.RetrieveUpdateDestroyAPIView):
+class UserAPICreate(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class PostAPIUpdate(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+
+class PostAPICreate(generics.CreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PointsAPIUpdate(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Points.objects.all()
+    serializer_class = PointsSerializer
+
+
+class PointsAPICreate(generics.CreateAPIView):
+    queryset = Points.objects.all()
+    serializer_class = PointsSerializer
+
+
+class CommentsAPIUpdate(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comments.objects.all()
+    serializer_class = CommentsSerializer
+
+
+class CommentsAPICreate(generics.CreateAPIView):
+    queryset = Comments.objects.all()
+    serializer_class = CommentsSerializer
+
+
+class PhotosAPIUpdate(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Photos.objects.all()
+    serializer_class = PhotosSerializer
+
+
+class PhotosAPICreate(generics.CreateAPIView):
+    queryset = Photos.objects.all()
+    serializer_class = PhotosSerializer
 
 
 def pageNotFound(request, exception):
