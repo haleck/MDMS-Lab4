fff37bf57541fa2449c3bcc31dddb79854314e25
Haleckiy <haleckiydenisirl@gmail.com>
2023-01-23 18:31:56 +0700

    Migration sqlite3 -> postgresql

--- mysocialnetwork/mysocialnetwork/settings.py
+++ mysocialnetwork/mysocialnetwork/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'mysocialnetwork.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'MDMS',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
